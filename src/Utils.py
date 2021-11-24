b2812647e53c3ae651b921ef5a62cba6ea91f4f3
Leo Zeitler <leozeitler95@gmail.com>
2021-11-24 16:14:35 +0100

    Update: argparse to MainUtils
    All argparse functions were moved to MainUtils module.
    The computation of frequency bins has been added.

--- src/Utils.py
+++ src/Utils.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-import argparse
+import numpy as np
 
 
 def validate_dir(rel_path=''):
@@ -15,63 +15,8 @@ def validate_dir(rel_path=''):
     return '%s/%s/' % (curr_dir, rel_path)
 
 
-def argparse_jmak_param(arguments):
-    parser = argparse.ArgumentParser(
-        description='Find the best JMAK parameters for CPD data and save them to file.'
-    )
-    parser.add_argument('--do_each', action='store_true', dest='do_each',
-                        help='If set, there is one model per region in each transcript. '
-                             'Otherwise beginning, centre of a gene and end are combined in one single model.')
-    parser.add_argument('--min_f_trans', type=float, default=.5,
-                        help='Minimum allowed value for maximum fraction in transcribed regions '
-                             'during parameter search.')
-    parser.add_argument('--min_f_igr', type=float, default=.4,
-                        help='Minimum allowed value for maximum fraction in intergenic regions '
-                             'during parameter search.')
-    parser.add_argument('--max_f', type=float, default=1.,
-                        help='Maximum allowed value for maximum fraction during parameter search.')
-    parser.add_argument('--delta_f', type=float, default=.01,
-                        help='Step size when increasing maximum fraction during parameter search.')
-    parser.add_argument('--num_cpus', type=int, default=1,
-                        help='Number of processes used for speeding up computations.')
-    parser.add_argument('--verbosity', type=int, default=2,
-                        help='Verbosity flag')
-    parser.add_argument('--save_fig', action='store_true', dest='save_fig',
-                        help='If set, figures are saved instead of displayed.')
-    parser.add_argument('--test', action='store_true', dest='test',
-                        help='Run test function for debugging.')
+def frequency_bins(x, nbin):
+    nlen = len(x)
+    return np.interp(np.linspace(0, nlen, nbin + 1), np.arange(nlen), np.sort(x))
 
-    parsed_args = parser.parse_args(arguments)
-    return parsed_args
-
-
-def argparse_bioplotter(arguments):
-    parser = argparse.ArgumentParser(
-        description='Plot available biological data wrt the parameter space. Train the JMAK model and save'
-                    'them to a file first.'
-    )
-
-    parser.add_argument('--bio_type', type=str, required=True,
-                        help='Pass the data type that is used in order to determine the colour gradient.'
-                             'Possible is: slam | nucl | size | meres'
-                        )
-    parser.add_argument('--use_sum', action='store_true', dest='use_sum',
-                        help='If set, when the biological data is loaded, the sum is taken instead of the mean.'
-                             'Only important for bio_type=slam or bio_type=nucl')
-    parser.add_argument('--do_each', action='store_true', dest='do_each',
-                        help='If set, there is one model per region in each transcript. '
-                             'Otherwise beginning, centre of a gene and end are combined in one single model.')
-    parser.add_argument('--save_fig', action='store_true', dest='save_fig',
-                        help='If set, figures are saved instead of displayed.')
-    parser.add_argument('--size_scaling', type=float, default=100.,
-                        help='Beta parameter is mulitplied by this value to determine the size.')
-    parser.add_argument('--size_power', type=float, default=3.,
-                        help='Scaled beta parameter is taken to the power of size_power to make small changes visible.')
-    parser.add_argument('--power_norm', type=float, default=1.,
-                        help='The colour gradient c is transformed c^power_norm.')
-    parser.add_argument('--save_prefix', type=str, default='',
-                        help='Set identifying string that goes in front of every saved file name.')
-
-    parsed_args = parser.parse_args(arguments)
-    return parsed_args
 
