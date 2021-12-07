5899531b27319149b0bc59ab117ab51fc6dced61
Leo Zeitler <leozeitler95@gmail.com>
2021-12-07 17:20:53 +0100

    Update: random train/test sets
    Instead of having pre-defined train and test chromosomes,
    train and test sets are determined randomly

--- predict.py
+++ predict.py
@@ -10,6 +10,7 @@ import pickle
 from src.DataLoader import load_chrom_data, load_chrom_split, load_bio_data, load_meres
 from src.UtilsMain import create_models, argparse_predict
 from src.Utils import validate_dir
+from src.Model import RegionModel
 from src.PredictionModels import GPParameterMap, KNNParameterMap, LinearParameterMap
 
 
@@ -44,118 +45,81 @@ SLAM_PATHS = [
 def convert_bio_data(
         bio_type,
         bio_index,
-        train_transcriptome,
-        test_transcriptome,
-        train_start_igr,
-        train_end_igr,
-        test_start_igr,
-        test_end_igr
+        transcriptome,
+        start_igr,
+        end_igr
 ):
-    train_trans, test_trans, train_igr, test_igr = None, None, None, None
+    trans_bio, igr_bio = None, None
     if bio_type.lower() == 'nucl' or bio_type == 'slam':
         bio_data_paths = NUCL_PATHS if bio_type.lower() == 'nucl' else SLAM_PATHS
         use_directionality = False if bio_type.lower() == 'nucl' else True
         idx = NUCLEOSOME_INDEX[bio_index] if bio_type.lower() == 'nucl' else SLAM_INDEX[bio_index]
-        train_trans, test_trans = load_bio_data(
-            zip(train_transcriptome['chr'].to_list(), train_transcriptome['start'].to_list()),
-            zip(train_transcriptome['chr'].to_list(), train_transcriptome['end'].to_list()),
-            zip(test_transcriptome['chr'].to_list(), test_transcriptome['start'].to_list()),
-            zip(test_transcriptome['chr'].to_list(), test_transcriptome['end'].to_list()),
+        trans_bio = load_bio_data(
+            zip(transcriptome['chr'].to_list(), transcriptome['start'].to_list()),
+            zip(transcriptome['chr'].to_list(), transcriptome['end'].to_list()),
             bio_data_paths,
             use_directionality=use_directionality
         )
-        train_igr, test_igr = load_bio_data(
-            train_start_igr,
-            train_end_igr,
-            test_start_igr,
-            test_end_igr,
+        igr_bio = load_bio_data(
+            start_igr,
+            end_igr,
             bio_data_paths,
             use_directionality=use_directionality
         )
-        train_trans, test_trans = train_trans[idx], test_trans[idx]
-        train_igr, test_igr = train_igr[idx], test_igr[idx]
+        trans_bio = trans_bio[idx]
+        igr_bio = igr_bio[idx]
 
     elif bio_type.lower() == 'size':
-        train_trans = np.abs(train_transcriptome['start'].to_numpy('int') - train_transcriptome['end'].to_numpy('int'))
-        test_trans = np.abs(test_transcriptome['start'].to_numpy('int') - test_transcriptome['end'].to_numpy('int'))
+        trans_bio = np.abs(transcriptome['start'].to_numpy('int') - transcriptome['end'].to_numpy('int'))
 
     elif bio_type.lower() == 'meres':
         c_pos = load_meres('centromeres')
         t_pos = load_meres('telomeres')
-        train_trans_diff_cen_tel = []
-        train_trans_chrom_compare = []
-        test_trans_diff_cen_tel = []
-        test_trans_chrom_compare = []
+        trans_diff_cen_tel = []
+        trans_chrom_compare = []
         for row in c_pos.iterrows():
             chrom = row[1]['chr']
             centromere = row[1]['pos']
             telomere = t_pos[t_pos['chr'] == chrom]['pos'].values[0]
-            train_trans_chrom = train_transcriptome[train_transcriptome['chr'] == chrom]
-            if len(train_trans_chrom) > 0:
-                centre = ((train_trans_chrom['start'] + train_trans_chrom['end']) / 2.).to_numpy('float')
-                train_trans_diff_cen_tel.extend(
+            trans_chrom = transcriptome[transcriptome['chr'] == chrom]
+            if len(trans_chrom) > 0:
+                centre = ((trans_chrom['start'] + trans_chrom['end']) / 2.).to_numpy('float')
+                trans_diff_cen_tel.extend(
                     np.minimum(np.abs(centre - centromere), np.abs(centre - telomere))
                 )
-                train_trans_chrom_compare.extend([chrom] * len(train_trans_chrom.index))
+                trans_chrom_compare.extend([chrom] * len(trans_chrom.index))
 
-            test_trans_chrom = test_transcriptome[test_transcriptome['chr'] == chrom]
-            if len(test_trans_chrom) > 0:
-                centre = ((test_trans_chrom['start'] + test_trans_chrom['end']) / 2.).to_numpy('float')
-                test_trans_diff_cen_tel.extend(
-                    np.minimum(np.abs(centromere - centre), np.abs(telomere - centre))
-                )
-                test_trans_chrom_compare.extend([chrom] * len(test_trans_chrom.index))
+        trans_bio = np.asarray(trans_diff_cen_tel)
+        if not np.all([x == y for x, y in zip(transcriptome['chr'].to_list(), trans_chrom_compare)]):
+            raise ValueError('Chromosomes (transcript) did not match')
 
-        train_trans = np.asarray(train_trans_diff_cen_tel)
-        test_trans = np.asarray(test_trans_diff_cen_tel)
-        if not np.all([x == y for x, y in zip(train_transcriptome['chr'].to_list(), train_trans_chrom_compare)]):
-            raise ValueError('Train chromosomes (transcript) did not match')
-        if not np.all([x == y for x, y in zip(test_transcriptome['chr'].to_list(), test_trans_chrom_compare)]):
-            raise ValueError('Test chromosomes (transcript) did not match')
-
-        train_igr_diff_cen_tel = []
-        train_igr_chrom_compare = []
-        test_igr_diff_cen_tel = []
-        test_igr_chrom_compare = []
+        igr_diff_cen_tel = []
+        igr_chrom_compare = []
         for row in c_pos.iterrows():
             chrom = row[1]['chr']
             centromere = row[1]['pos']
             telomere = t_pos[t_pos['chr'] == chrom]['pos'].values[0]
 
-            chrom_list = list(filter(lambda x: x[0] == chrom, train_start_igr))
+            chrom_list = list(filter(lambda x: x[0] == chrom, start_igr))
             if chrom_list:
                 train_chrom_igr, train_chrom_igr_start = zip(*chrom_list)
-                _, train_chrom_igr_end = zip(*filter(lambda x: x[0] == chrom, train_end_igr))
+                _, train_chrom_igr_end = zip(*filter(lambda x: x[0] == chrom, end_igr))
                 centre = (np.asarray(train_chrom_igr_start, dtype='float')
                           + np.asarray(train_chrom_igr_end, dtype='float')) / 2.
-                train_igr_diff_cen_tel.extend(
+                igr_diff_cen_tel.extend(
                     np.minimum(np.abs(centre - centromere), np.abs(centre - telomere))
                 )
-                train_igr_chrom_compare.extend(train_chrom_igr)
+                igr_chrom_compare.extend(train_chrom_igr)
 
-            chrom_list = list(filter(lambda x: x[0] == chrom, test_start_igr))
-            if chrom_list:
-                test_chrom_igr, test_chrom_igr_start = zip(*chrom_list)
-                _, test_chrom_igr_end = zip(*filter(lambda x: x[0] == chrom, test_end_igr))
-                centre = (np.asarray(test_chrom_igr_start, dtype='float')
-                          + np.asarray(test_chrom_igr_end, dtype='float')) / 2.
-                test_igr_diff_cen_tel.extend(
-                    np.minimum(np.abs(centre - centromere), np.abs(centre - telomere))
-                )
-                test_igr_chrom_compare.extend(test_chrom_igr)
+        chr_igr, _ = zip(*start_igr)
+        if not np.all([x == y for x, y in zip(chr_igr, igr_chrom_compare)]):
+            raise ValueError('Chromosomes (IGR) did not match')
 
-        chr_train, _ = zip(*train_start_igr)
-        chr_test, _ = zip(*test_start_igr)
-        if not np.all([x == y for x, y in zip(chr_train, train_igr_chrom_compare)]):
-            raise ValueError('Train chromosomes (IGR) did not match')
-        if not np.all([x == y for x, y in zip(chr_test, test_igr_chrom_compare)]):
-            raise ValueError('Test chromosomes (IGR) did not match')
-        train_igr = np.asarray(train_igr_diff_cen_tel)
-        test_igr = np.asarray(test_igr_diff_cen_tel)
+        igr_bio = np.asarray(igr_diff_cen_tel)
     else:
         raise ValueError('Unknown bio type.')
 
-    return train_trans, test_trans, train_igr, test_igr
+    return trans_bio, igr_bio
 
 
 def main_nucl(args):
@@ -173,54 +137,54 @@ def main_nucl(args):
     rm_percentile = args.rm_percentile
     neg_random = args.neg_random
     num_classes = args.num_classes
-    k_neighbours = args.kneighbors
+    k_neighbours = args.kneighbour
+    test_ratio = .3
 
     if verbosity > 0:
         print('Load CPD')
-    train_chrom = load_chrom_split('train')
-    test_chrom = load_chrom_split('test')
+    chrom_list = load_chrom_split()
 
-    train_data, test_data = load_chrom_data(train_chrom_list=train_chrom, test_chrom_list=test_chrom)
-    region_model_list = create_models(train_data, test_data, do_each=do_each)
+    data = load_chrom_data(chrom_list=chrom_list)
+    region_model_list = create_models(data, do_each=do_each)
 
-    (_, _, _, train_start_igr, train_end_igr, train_transcriptome) = train_data
-    (_, _, _, test_start_igr, test_end_igr, test_transcriptome) = test_data
+    (_, _, _, start_igr, end_igr, transcriptome) = data
 
     if verbosity > 0:
         print('Load bio data')
         print('Bio type: %s' % bio_type)
 
-    train_trans, test_trans, train_igr, test_igr = convert_bio_data(
+    trans_bio, igr_bio = convert_bio_data(
         bio_type,
         bio_index,
-        train_transcriptome,
-        test_transcriptome,
-        train_start_igr,
-        train_end_igr,
-        test_start_igr,
-        test_end_igr
+        transcriptome,
+        start_igr,
+        end_igr
     )
 
     if bio_type.lower() in ['slam', 'size']:
         region_model_list = list(filter(lambda x: 'gene' in x.name.lower() or 'nts' in x.name.lower(), region_model_list))
     train_data, test_data = [], []
     for rm in region_model_list:
+        mask = np.zeros(len(rm.models), dtype='bool')
+        mask[np.random.choice(mask.size, size=np.round(mask.size * test_ratio).astype('int'), replace=False)] = True
+        test_m = np.asarray(list(rm.get_model_parameter('m', do_filter=False)))[mask]
+        test_mf = np.asarray(list(rm.get_model_parameter('max_frac', do_filter=False)))[mask]
+        test_beta = np.asarray(list(rm.get_model_parameter('beta', do_filter=False)))[mask]
+        rm.models = [jmak_model for num, jmak_model in enumerate(rm.models) if not mask[num]]
+        test_params = np.asarray([test_m, test_mf, test_beta]).T
         if 'gene' in rm.name.lower() or 'nts' in rm.name.lower():
-            traind, testd = train_trans, test_trans
+            traind, testd = trans_bio[~mask], trans_bio[mask]
         else:
-            traind, testd = train_igr, test_igr
+            traind, testd = igr_bio[~mask], igr_bio[mask]
 
-        if 'test' in rm.name.lower():
-            test_data.append((rm, testd))
-        else:
-            train_data.append((rm, traind))
-
+        train_data.append((rm, traind))
+        test_data.append((test_params, testd))
     ml_models = []
     if verbosity > 0:
         print('Learn models')
         if neg_random:
             print('Randomise model')
-    for rm, traind in [train_data]:  # TODO
+    for rm, traind in train_data:
         if verbosity > 0:
             print('Create parameter mapping for model %s' % rm.name)
         temp_m = np.asarray(list(rm.get_model_parameter('m', do_filter=False)))
@@ -291,7 +255,7 @@ def main_nucl(args):
             save_fig=save_fig,
             save_prefix=save_prefix,
             plotted_dp=plotted_dp,
-            levels=num_classes
+            levels=num_classes if ml_type.lower() in ['knn', 'lin'] else 15
         )
 
         mean, var = b_model.estimate(b_model.data_params, convert_dp=False)
@@ -303,20 +267,17 @@ def main_nucl(args):
             convert_params=False,
             convert_val=False,
             save_fig=save_fig,
-            save_prefix=save_prefix
+            save_prefix='%s_train' % save_prefix
         )
         ml_models.append(b_model)
 
     if verbosity > 0:
         print('Predict')
 
-    for num, (ml, (rm, testd)) in enumerate(zip(ml_models, [test_data])):  # TODO
+    for num, (ml, (input_param, testd)) in enumerate(zip(ml_models, test_data)):
         if verbosity > 1:
-            print('Predict model %s' % rm.name)
-
-        input_m = np.asarray(list(rm.get_model_parameter('m', do_filter=False)))
-        input_beta = np.asarray(list(rm.get_model_parameter('beta', do_filter=False)))
-        input_mf = np.asarray(list(rm.get_model_parameter('max_frac', do_filter=False)))
+            print('Predict model %s' % ml.rmodel.name)
+        input_m, input_mf, input_beta = input_param[:, 0], input_param[:, 1], input_param[:, 2]
         mask = ~np.isnan(input_m)
         mask = np.logical_and(input_m < 6., mask)
         mask = np.logical_and(input_beta < .05, mask)
@@ -329,7 +290,7 @@ def main_nucl(args):
             params,
             convert_val=True if ml_type.lower() in ['knn', 'lin'] else False,
             save_fig=save_fig,
-            save_prefix=save_prefix
+            save_prefix='%s_test' % save_prefix
         )
         if save_fig:
             directory = validate_dir('arrays')
