aacd74fb812130f1997e9188b9e3d2efcd652695
Leo Zeitler <leozeitler95@gmail.com>
2021-12-17 12:14:45 +0100

    Update: new data types
    Changed slam to net-seq. Added abf1 and gamma h2ax

--- bioPlotting.py
+++ bioPlotting.py
@@ -16,14 +16,22 @@ def main(args):
     bio_type = args.bio_type
     save_prefix = args.save_prefix
     use_sum = args.use_sum
+    no_tcr = args.no_tcr
+    used_transcriptoms = [True, False, False] if not no_tcr else [True, True, True]
+    num_bins = 3 if not no_tcr else 1
 
-    slam_paths = [
-        'data/seq/nouv_slam_mins_new.bw',
-        'data/seq/nouv_slam_plus_new.bw',
-        'data/seq/20m_slam_mins_new.bw',
-        'data/seq/20m_slam_plus_new.bw',
-        'data/seq/120m_slam_mins_new.bw',
-        'data/seq/120m_slam_plus_new.bw'
+    netseq_paths = [
+        'data/seq/wt_netseq_minus.bw',
+        'data/seq/wt_netseq_plus.bw'
+    ]
+
+    abf1_paths = [
+        'data/seq/wt_abf1_nouv.bw',
+        'data/seq/wt_abf1_uv.bw'
+    ]
+
+    h2aX_paths = [
+        'data/seq/wt_h2a_0m.bw'
     ]
 
     nucl_paths = [
@@ -34,40 +42,40 @@ def main(args):
 
     print('Load CPD')
     chrom_list = load_chrom_split()
-    data = load_chrom_data(chrom_list=chrom_list)
-    region_model_list = create_models(data, do_each=do_each)
+    data = load_chrom_data(chrom_list=chrom_list, used_transcriptomes=used_transcriptoms, num_trans_bins=num_bins)
+    region_model_list = create_models(data, do_each=do_each, no_tcr=no_tcr)
 
     (_, _, _, start_igr, end_igr, transcriptome) = data
 
     trans_rmodels = list(filter(lambda x: 'gene' in x.name.lower() or 'nts' in x.name.lower(), region_model_list))
     igr_rmodels = list(filter(lambda x: 'intergenic' in x.name.lower(), region_model_list))
 
-    if bio_type.lower() == 'slam':
-        # SLAM-seq data
-        print('Load SLAM-seq')
-        slam_data = load_bio_data(
+    if bio_type.lower() == 'netseq':
+        # NET-seq data
+        print('Load NET-seq')
+        netseq_data = load_bio_data(
             zip(transcriptome['chr'].to_list(), transcriptome['start'].to_list()),
             zip(transcriptome['chr'].to_list(), transcriptome['end'].to_list()),
-            slam_paths,
+            netseq_paths,
             use_directionality=True,
             use_sum=use_sum
         )
 
         for tm in trans_rmodels:
-            for sd, n in zip(slam_data, ['nouv', '20m', '120m']):
+            for netd in netseq_data:
                 for mr in [(0, 5), (5, 10), (10, 15), (15, 20), (20, 25)]:
                     tm.plot_parameter_with_gradient(
-                        sd,
+                        netd,
                         cmap='PiYG',
                         alpha=.7,
                         m_range=mr,
-                        title_add=n,
+                        title_add='| NET-seq',
                         size_scaling=size_scaling,
                         size_power=size_power,
                         power_norm=power_norm,
                         num_handles=8,
                         save_fig=save_fig,
-                        save_prefix='%s_slam_%s_%s_%s' % (save_prefix, n, mr[0], mr[1])
+                        save_prefix='%s_netseq_%s_%s' % (save_prefix, mr[0], mr[1])
                     )
 
     elif bio_type.lower() == 'nucl':
@@ -96,7 +104,7 @@ def main(args):
                         cmap='BrBG',
                         alpha=.7,
                         m_range=mr,
-                        title_add=n,
+                        title_add='| Nucleosome density %s' % n,
                         size_scaling=size_scaling,
                         size_power=size_power,
                         power_norm=power_norm,
@@ -112,7 +120,7 @@ def main(args):
                         cmap='BrBG',
                         alpha=.7,
                         m_range=mr,
-                        title_add=n,
+                        title_add='| Nucleosome density %s' % n,
                         size_scaling=size_scaling,
                         size_power=size_power,
                         power_norm=power_norm,
@@ -130,6 +138,7 @@ def main(args):
                     size_list,
                     cmap='Spectral',
                     alpha=.7,
+                    title_add='| Size',
                     m_range=mr,
                     size_scaling=size_scaling,
                     size_power=size_power,
@@ -138,28 +147,156 @@ def main(args):
                     save_prefix='%s_size_%s_%s' % (save_prefix, mr[0], mr[1])
                 )
 
-    elif bio_type.lower() == 'meres':
+    elif bio_type.lower() == 'abf1':
+        # Load abf1 data
+        abf1_data_trans = load_bio_data(
+            zip(transcriptome['chr'].to_list(), transcriptome['start'].to_list()),
+            zip(transcriptome['chr'].to_list(), transcriptome['end'].to_list()),
+            abf1_paths,
+            use_directionality=False,
+            use_sum=use_sum
+        )
+
+        abf1_data_igr = load_bio_data(
+            start_igr,
+            end_igr,
+            abf1_paths,
+            use_directionality=False,
+            use_sum=use_sum
+        )
+
+        for tm in trans_rmodels:
+            for ad, n in zip(abf1_data_trans, ['nouv', '0m']):
+                for mr in [(0, 5), (5, 10), (10, 15), (15, 20), (20, 25)]:
+                    tm.plot_parameter_with_gradient(
+                        ad,
+                        cmap='PuOr',
+                        alpha=.7,
+                        m_range=mr,
+                        title_add='| Abf1 %s' % n,
+                        size_scaling=size_scaling,
+                        size_power=size_power,
+                        power_norm=power_norm,
+                        save_fig=save_fig,
+                        save_prefix='%s_abf1_%s_%s_%s' % (save_prefix, n, mr[0], mr[1])
+                    )
+
+        for tm in igr_rmodels:
+            for ad, n in zip(abf1_data_igr, ['nouv', '0m']):
+                for mr in [(0, 5), (5, 10), (10, 15), (15, 20), (20, 25)]:
+                    tm.plot_parameter_with_gradient(
+                        ad,
+                        cmap='PuOr',
+                        alpha=.7,
+                        m_range=mr,
+                        title_add='| Abf1 %s' % n,
+                        size_scaling=size_scaling,
+                        size_power=size_power,
+                        power_norm=power_norm,
+                        save_fig=save_fig,
+                        save_prefix='%s_abf1_%s_%s_%s' % (save_prefix, n, mr[0], mr[1])
+                    )
+    elif bio_type.lower() == 'h2a':
+        # Load gamma h2aX data
+        h2aX_data_trans = load_bio_data(
+            zip(transcriptome['chr'].to_list(), transcriptome['start'].to_list()),
+            zip(transcriptome['chr'].to_list(), transcriptome['end'].to_list()),
+            h2aX_paths,
+            use_directionality=False,
+            use_sum=use_sum
+        )
+
+        h2aX_data_igr = load_bio_data(
+            start_igr,
+            end_igr,
+            h2aX_paths,
+            use_directionality=False,
+            use_sum=use_sum
+        )
+
+        for tm in trans_rmodels:
+            for mr in [(0, 5), (5, 10), (10, 15), (15, 20), (20, 25)]:
+                tm.plot_parameter_with_gradient(
+                    h2aX_data_trans[0],
+                    cmap='coolwarm',
+                    alpha=.7,
+                    title_add='| gamma H2AX',
+                    m_range=mr,
+                    size_scaling=size_scaling,
+                    size_power=size_power,
+                    power_norm=power_norm,
+                    save_fig=save_fig,
+                    save_prefix='%s_gh2ax_%s_%s' % (save_prefix, mr[0], mr[1])
+                )
+
+        for im in igr_rmodels:
+            for mr in [(0, 5), (5, 10), (10, 15), (15, 20), (20, 25)]:
+                im.plot_parameter_with_gradient(
+                    h2aX_data_igr[0],
+                    cmap='coolwarm',
+                    alpha=.7,
+                    title_add='| gamma H2AX',
+                    m_range=mr,
+                    size_scaling=size_scaling,
+                    size_power=size_power,
+                    power_norm=power_norm,
+                    save_fig=save_fig,
+                    save_prefix='%s_gh2ax_%s_%s' % (save_prefix, mr[0], mr[1])
+                )
+
+    elif 'meres' in bio_type.lower():
         # Telomeres and centromeres
-        print('Telomeres and centromeres')
+        split = bio_type.lower().split('_')
+        do_relative = False
+        if len(split) == 2 and split[0] == 'rel':
+            do_relative = True
         c_pos = load_meres('centromeres')
         t_pos = load_meres('telomeres')
-        trans_diff_cen_tel = []
-        trans_chrom_compare = []
+        trans_diff_cen_tel, trans_chrom_compare = [], []
+        igr_diff_cen_tel, igr_chrom_compare = [], []
         for row in c_pos.iterrows():
             chrom = row[1]['chr']
             centromere = row[1]['pos']
-            telomere = t_pos[t_pos['chr'] == chrom]['pos'].values[0]
+            telomere = t_pos[t_pos['chr'] == chrom]['pos'].values
+            if do_relative:
+                denominator = np.maximum(np.abs(centromere - telomere[0]), np.abs(centromere - telomere[1])) / 2.
+
             trans_chrom = transcriptome[transcriptome['chr'] == chrom]
             if len(trans_chrom) > 0:
                 centre = ((trans_chrom['start'] + trans_chrom['end']) / 2.).to_numpy('float')
-                trans_diff_cen_tel.extend(
-                    np.minimum(np.abs(centre - centromere), np.abs(centre - telomere))
+                distance = np.minimum(
+                    np.minimum(np.abs(centre - centromere), np.abs(centre - telomere[0])),
+                    np.abs(centre - telomere[1])
                 )
+                if do_relative:
+                    distance /= denominator
+                trans_diff_cen_tel.extend(distance)
                 trans_chrom_compare.extend([chrom] * len(trans_chrom.index))
+
+            chrom_list = list(filter(lambda x: x[0] == chrom, start_igr))
+            if chrom_list:
+                train_chrom_igr, train_chrom_igr_start = zip(*chrom_list)
+                _, train_chrom_igr_end = zip(*filter(lambda x: x[0] == chrom, end_igr))
+                centre = (np.asarray(train_chrom_igr_start, dtype='float')
+                          + np.asarray(train_chrom_igr_end, dtype='float')) / 2.
+                distance = np.minimum(
+                    np.minimum(np.abs(centre - centromere), np.abs(centre - telomere[0])),
+                    np.abs(centre - telomere[1])
+                )
+                if do_relative:
+                    distance /= denominator
+                igr_diff_cen_tel.extend(distance)
+                igr_chrom_compare.extend(train_chrom_igr)
+
+        trans_diff_cen_tel = np.asarray(trans_diff_cen_tel)
         if not np.all([x == y for x, y in zip(transcriptome['chr'].to_list(), trans_chrom_compare)]):
             raise ValueError('Chromosomes (transcript) did not match')
 
-        trans_diff_cen_tel = np.asarray(trans_diff_cen_tel)
+        chr_igr, _ = zip(*start_igr)
+        if not np.all([x == y for x, y in zip(chr_igr, igr_chrom_compare)]):
+            raise ValueError('Chromosomes (IGR) did not match')
+
+        igr_diff_cen_tel = np.asarray(igr_diff_cen_tel)
         for tm in trans_rmodels:
             for mr in [(0, 5), (5, 10), (10, 15), (15, 20), (20, 25)]:
                 tm.plot_parameter_with_gradient(
@@ -167,34 +304,13 @@ def main(args):
                     cmap='RdGy',
                     alpha=.7,
                     m_range=mr,
+                    title_add='| meres' if bio_type.lower() == 'meres' else '| Relative meres',
                     size_scaling=size_scaling,
                     size_power=size_power,
                     power_norm=power_norm,
                     save_fig=save_fig,
                     save_prefix='%s_meres_%s_%s' % (save_prefix, mr[0], mr[1])
                 )
-
-        igr_diff_cen_tel = []
-        igr_chrom_compare = []
-        for row in c_pos.iterrows():
-            chrom = row[1]['chr']
-            centromere = row[1]['pos']
-            telomere = t_pos[t_pos['chr'] == chrom]['pos'].values[0]
-
-            chrom_list = list(filter(lambda x: x[0] == chrom, start_igr))
-            if chrom_list:
-                chrom_igr, chrom_igr_start = zip(*chrom_list)
-                _, train_chrom_igr_end = zip(*filter(lambda x: x[0] == chrom, end_igr))
-                centre = (np.asarray(chrom_igr_start, dtype='float')
-                          + np.asarray(train_chrom_igr_end, dtype='float')) / 2.
-                igr_diff_cen_tel.extend(
-                    np.minimum(np.abs(centre - centromere), np.abs(centre - telomere))
-                )
-                igr_chrom_compare.extend(chrom_igr)
-
-        chr_igr, _ = zip(*start_igr)
-        if not np.all([x == y for x, y in zip(chr_igr, igr_chrom_compare)]):
-            raise ValueError('Chromosomes (IGR) did not match')
 
         igr_diff_cen_tel = np.asarray(igr_diff_cen_tel)
         for tm in igr_rmodels:
@@ -204,12 +320,17 @@ def main(args):
                     cmap='RdGy',
                     alpha=.7,
                     m_range=mr,
+                    title_add='| meres' if bio_type.lower() == 'meres' else '| Relative meres',
                     size_scaling=size_scaling,
                     size_power=size_power,
                     power_norm=power_norm,
                     save_fig=save_fig,
-                    save_prefix='%s_meres_%s_%s' % (save_prefix, mr[0], mr[1])
+                    save_prefix='%s_%s_%s_%s' % (save_prefix, bio_type.lower(), mr[0], mr[1])
                 )
+
+    else:
+        raise ValueError('Bio data identifier not understood. Choose between: '
+                         'netseq | nucl | size | abf1 | h2a | meres | rel_meres')
 
 
 if __name__ == '__main__':
