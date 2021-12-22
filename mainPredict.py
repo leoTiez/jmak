00ef75e2d8b2dde9f7db192955e9fe5c684c4c8f
Leo Zeitler <leozeitler95@gmail.com>
2021-12-22 11:27:51 +0100

    Bugfix: wrong indent
    Was not looping over both setups. This is fixed

--- mainPredict.py
+++ mainPredict.py
@@ -16,136 +16,137 @@ def main():
             else:
                 do_each = False
                 no_tcr = True
-        for bt in bio_types:
-            if bt == 'abf1':
-                bi = 'uv'
-            elif bt == 'nucl':
-                bi = '0min'
-            else:
-                bi = ''
+                
+            for bt in bio_types:
+                if bt == 'abf1':
+                    bi = 'uv'
+                elif bt == 'nucl':
+                    bi = '0min'
+                else:
+                    bi = ''
 
-            if bt == 'nucl':
-                rm_percentile = 0.
-            else:
-                rm_percentile = 5.
+                if bt == 'nucl':
+                    rm_percentile = 0.
+                else:
+                    rm_percentile = 5.
 
-            for i in range(1, num_trials + 1):
-                if include_lin:
-                    # Lin
-                    parallel.apply_async(
-                        os.system, ('python3.8 predict.py '
-                              '--bio_type=%s '
-                              '--bio_index=%s '
-                              '--ml_type=lin '
-                              '--num_classes=%s '
-                              '--rm_percentile=%s '
-                              '%s '
-                              '%s '
-                              '--save_prefix=%s%s_lin_%s%s '
-                              '--save_fig '
-                              '--to_pickle '
-                              '--num_cpus=1 '
-                              '--verbosity=4 '
-                              % (
-                                  bt,
-                                  bi,
-                                  num_classes,
-                                  rm_percentile,
-                                  '--do_each' if do_each else '',
-                                  '--no_tcr' if no_tcr else '',
-                                  'notcr_' if no_tcr else '',
-                                  bt,
-                                  'each' if do_each else 'total',
-                                  i
-                              ), ))
-                    parallel.apply_async(
-                        os.system, ('python3.8 predict.py '
-                              '--bio_type=%s '
-                              '--bio_index=%s '
-                              '--ml_type=lin '
-                              '--num_classes=%s '
-                              '--rm_percentile=%s '
-                              '%s '
-                              '%s '
-                              '--neg_random '
-                              '--save_prefix=%s%s_lin_%s%s_random '
-                              '--save_fig '
-                              '--to_pickle '
-                              '--num_cpus=1 '
-                              '--verbosity=4 '
-                              % (
-                                  bt,
-                                  bi,
-                                  num_classes,
-                                  rm_percentile,
-                                  '--do_each' if do_each else '',
-                                  '--no_tcr' if no_tcr else '',
-                                  'notcr_' if no_tcr else '',
-                                  bt,
-                                  'each' if do_each else 'total',
-                                  i
-                              ), ))
-                # kNN
-                for kneighbour in [5, 10, 20, 50, 100]:
-                    parallel.apply_async(
-                        os.system, ('python3.8 predict.py '
-                              '--bio_type=%s '
-                              '--bio_index=%s '
-                              '--ml_type=knn '
-                              '--num_classes=%s '
-                              '--rm_percentile=%s '
-                              '--kneighbour=%s '
-                              '%s '
-                              '%s '
-                              '--save_prefix=%s%s_knn%s_%s%s '
-                              '--save_fig '
-                              '--to_pickle '
-                              '--num_cpus=1 '
-                              '--verbosity=4 '
-                              % (
-                                  bt,
-                                  bi,
-                                  num_classes,
-                                  rm_percentile,
-                                  kneighbour,
-                                  '--do_each' if do_each else '',
-                                  '--no_tcr' if no_tcr else '',
-                                  'notcr_' if no_tcr else '',
-                                  bt,
-                                  kneighbour,
-                                  'each' if do_each else 'total',
-                                  i
-                              ), ))
-                    parallel.apply_async(
-                        os.system, ('python3.8 predict.py '
-                              '--bio_type=%s '
-                              '--bio_index=%s '
-                              '--ml_type=knn '
-                              '--num_classes=%s '
-                              '--rm_percentile=%s '
-                              '--kneighbour=%s '
-                              '%s '
-                              '%s '
-                              '--neg_random '
-                              '--save_prefix=%s%s_knn%s_%s%s_random '
-                              '--save_fig '
-                              '--to_pickle '
-                              '--num_cpus=1 '
-                              '--verbosity=4 '
-                              % (
-                                  bt,
-                                  bi,
-                                  num_classes,
-                                  rm_percentile,
-                                  kneighbour,
-                                  '--do_each' if do_each else '',
-                                  '--no_tcr' if no_tcr else '',
-                                  'notcr_' if no_tcr else '',
-                                  bt,
-                                  kneighbour,
-                                  'each' if do_each else 'total',
-                                  i
-                              ), ))
+                for i in range(1, num_trials + 1):
+                    if include_lin:
+                        # Lin
+                        parallel.apply_async(
+                            os.system, ('python3.8 predict.py '
+                                  '--bio_type=%s '
+                                  '--bio_index=%s '
+                                  '--ml_type=lin '
+                                  '--num_classes=%s '
+                                  '--rm_percentile=%s '
+                                  '%s '
+                                  '%s '
+                                  '--save_prefix=%s%s_lin_%s%s '
+                                  '--save_fig '
+                                  '--to_pickle '
+                                  '--num_cpus=1 '
+                                  '--verbosity=4 '
+                                  % (
+                                      bt,
+                                      bi,
+                                      num_classes,
+                                      rm_percentile,
+                                      '--do_each' if do_each else '',
+                                      '--no_tcr' if no_tcr else '',
+                                      'notcr_' if no_tcr else '',
+                                      bt,
+                                      'each' if do_each else 'total',
+                                      i
+                                  ), ))
+                        parallel.apply_async(
+                            os.system, ('python3.8 predict.py '
+                                  '--bio_type=%s '
+                                  '--bio_index=%s '
+                                  '--ml_type=lin '
+                                  '--num_classes=%s '
+                                  '--rm_percentile=%s '
+                                  '%s '
+                                  '%s '
+                                  '--neg_random '
+                                  '--save_prefix=%s%s_lin_%s%s_random '
+                                  '--save_fig '
+                                  '--to_pickle '
+                                  '--num_cpus=1 '
+                                  '--verbosity=4 '
+                                  % (
+                                      bt,
+                                      bi,
+                                      num_classes,
+                                      rm_percentile,
+                                      '--do_each' if do_each else '',
+                                      '--no_tcr' if no_tcr else '',
+                                      'notcr_' if no_tcr else '',
+                                      bt,
+                                      'each' if do_each else 'total',
+                                      i
+                                  ), ))
+                    # kNN
+                    for kneighbour in [5, 10, 20, 50, 100]:
+                        parallel.apply_async(
+                            os.system, ('python3.8 predict.py '
+                                  '--bio_type=%s '
+                                  '--bio_index=%s '
+                                  '--ml_type=knn '
+                                  '--num_classes=%s '
+                                  '--rm_percentile=%s '
+                                  '--kneighbour=%s '
+                                  '%s '
+                                  '%s '
+                                  '--save_prefix=%s%s_knn%s_%s%s '
+                                  '--save_fig '
+                                  '--to_pickle '
+                                  '--num_cpus=1 '
+                                  '--verbosity=4 '
+                                  % (
+                                      bt,
+                                      bi,
+                                      num_classes,
+                                      rm_percentile,
+                                      kneighbour,
+                                      '--do_each' if do_each else '',
+                                      '--no_tcr' if no_tcr else '',
+                                      'notcr_' if no_tcr else '',
+                                      bt,
+                                      kneighbour,
+                                      'each' if do_each else 'total',
+                                      i
+                                  ), ))
+                        parallel.apply_async(
+                            os.system, ('python3.8 predict.py '
+                                  '--bio_type=%s '
+                                  '--bio_index=%s '
+                                  '--ml_type=knn '
+                                  '--num_classes=%s '
+                                  '--rm_percentile=%s '
+                                  '--kneighbour=%s '
+                                  '%s '
+                                  '%s '
+                                  '--neg_random '
+                                  '--save_prefix=%s%s_knn%s_%s%s_random '
+                                  '--save_fig '
+                                  '--to_pickle '
+                                  '--num_cpus=1 '
+                                  '--verbosity=4 '
+                                  % (
+                                      bt,
+                                      bi,
+                                      num_classes,
+                                      rm_percentile,
+                                      kneighbour,
+                                      '--do_each' if do_each else '',
+                                      '--no_tcr' if no_tcr else '',
+                                      'notcr_' if no_tcr else '',
+                                      bt,
+                                      kneighbour,
+                                      'each' if do_each else 'total',
+                                      i
+                                  ), ))
 
         parallel.close()
         parallel.join()
